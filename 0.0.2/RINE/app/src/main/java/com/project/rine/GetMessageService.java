e87077ffe2a29d23fd003af130bc6c6dee6bf59b
SageSeven <fanhua123@pku.edu.cn>
2020-01-10 16:45:34 +0800

    1

--- 0.0.2/RINE/app/src/main/java/com/project/rine/GetMessageService.java
+++ 0.0.2/RINE/app/src/main/java/com/project/rine/GetMessageService.java
@@ -2,15 +2,26 @@ package com.project.rine;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class GetMessageService extends Service {
+
+    private GetMessageBinder mBinder = new GetMessageBinder();
+
+    class GetMessageBinder extends Binder {
+        public String getMessage() {
+            gV.xLog("RINE_debug", "Getting message.");
+            return "";
+        }
+    }
+
+
     public GetMessageService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
